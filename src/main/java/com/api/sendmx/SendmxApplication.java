d86230bd10e4c76e8a3ce3d2e13f8ff8db9579df
jaoluiz13 <luizcostajoao12@gmail.com>
2023-07-04 13:22:17 -0300

    repo initializing

---
+++ src/main/java/com/api/sendmx/SendmxApplication.java
@@ -0,0 +1,13 @@
+package com.api.sendmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendmxApplication.class, args);
+	}
+
+}
